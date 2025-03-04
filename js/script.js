252f6de2139c08f28400ea1951959de974d1e89e
Olexandr <sashokseven@gmail.com>
2025-03-04 23:41:45 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const swiper = new Swiper('.materials__slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  });
