86519fcd5e051bae138201a66aa9ae5580b778ee
Olexandr <sashokseven@gmail.com>
2025-03-08 19:00:15 +0200

    fix header bg after scroll

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+const swiper = new Swiper(".materials__slider", {
+  // Optional parameters
+  loop: true,
 
-const swiper = new Swiper('.materials__slider', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-  });
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-  window.addEventListener('scroll', function(){
-    scrollY > 0?document.querySelector('.header').classList.add('scroll'): document.querySelector('.header').classList.remove('scroll')
-  })
+window.addEventListener("scroll", function () {
+  scrollY > 0
+    ? document.querySelector(".header").classList.add("scroll")
+    : document.querySelector(".header").classList.remove("scroll");
+});
